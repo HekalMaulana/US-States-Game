2ce9a37a0a8eef610096f96a43f82fff80c163b1
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2023-07-12 00:31:22 +0800

    U.S states game created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 IMAGE = "blank_states_img.gif"
 screen = turtle.Screen()
@@ -9,7 +10,27 @@ screen.setup(800, 600)
 screen.bgcolor("black")
 turtle.shape(IMAGE)
 
-answer_states = screen.textinput(title="Guess a U.S States", prompt="Guess another U.S States ? ")
+data = pandas.read_csv("50_states.csv")
+us_state = data["state"]
+us_state_list = us_state.tolist()
+
+correct_guess = []
+
+while len(correct_guess) < 50:
+    answer_states = screen.textinput(title=f"{len(correct_guess)}/50 States Correct", prompt="Guess another U.S States ? ").title()
+
+    if answer_states in us_state_list:
+        correct_guess.append(answer_states)
+        state = data[data["state"] == answer_states]
+        x_state = state["x"].iloc[0]
+        y_state = state["y"].iloc[0]
+
+        text = turtle.Turtle()
+        text.color("black")
+        text.hideturtle()
+        text.penup()
+        text.goto(x_state, y_state)
+        text.write(arg=f"{answer_states}")
 
 
 screen.exitonclick()
