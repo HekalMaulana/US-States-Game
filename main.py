2774721564842f6a4c1b1bd02954180c92f8a126
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2023-07-12 08:23:32 +0800

    Created new file that contain not answer state

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ us_state = data["state"]
 us_state_list = us_state.tolist()
 
 correct_guess = []
+missing_state = []
 
 while len(correct_guess) < 50:
     answer_states = screen.textinput(title=f"{len(correct_guess)}/50 States Correct", prompt="Guess another U.S States ? ").title()
+
+    if answer_states == "Exit":
+        break
 
     if answer_states in us_state_list:
         correct_guess.append(answer_states)
@@ -31,6 +35,18 @@ while len(correct_guess) < 50:
         text.penup()
         text.goto(x_state, y_state)
         text.write(arg=f"{answer_states}")
+
+for state_data in us_state_list:
+    if state_data not in correct_guess:
+        missing_state.append(state_data)
+
+states_to_learn = pandas.DataFrame(missing_state)
+states_to_learn.to_csv("states_to_learn.csv")
+
+
+
+
+
 
 
 screen.exitonclick()
