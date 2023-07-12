24640fc17531d4f29f755a40d66545e0ce58bdda
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2023-07-13 02:08:41 +0800

    Modify the exit program with list comperhension

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ us_state = data["state"]
 us_state_list = us_state.tolist()
 
 correct_guess = []
-missing_state = []
 
 while len(correct_guess) < 50:
-    answer_states = screen.textinput(title=f"{len(correct_guess)}/50 States Correct", prompt="Guess another U.S States ? ").title()
+    answer_states = screen.textinput(title=f"{len(correct_guess)}/50 States Correct",
+                                     prompt="Guess another U.S States ? ").title()
 
     if answer_states == "Exit":
+        missing_state = [state for state in us_state_list if state not in correct_guess]
+        states_to_learn = pandas.DataFrame(missing_state)
+        states_to_learn.to_csv("states_to_learn.csv")
         break
 
     if answer_states in us_state_list:
@@ -35,18 +38,5 @@ while len(correct_guess) < 50:
         text.penup()
         text.goto(x_state, y_state)
         text.write(arg=f"{answer_states}")
-
-for state_data in us_state_list:
-    if state_data not in correct_guess:
-        missing_state.append(state_data)
-
-states_to_learn = pandas.DataFrame(missing_state)
-states_to_learn.to_csv("states_to_learn.csv")
-
-
-
-
-
-
 
 screen.exitonclick()
